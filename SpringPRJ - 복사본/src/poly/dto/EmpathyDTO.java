6c06b3280d200fdc8c624d2e123ef47b506fae18
조정규 <107wjd@naver.com>
2020-06-28 23:28:16 +0900

    0.2.7

---
+++ SpringPRJ - 복사본/src/poly/dto/EmpathyDTO.java
@@ -0,0 +1,15 @@
+package poly.dto;
+
+public class EmpathyDTO {
+
+	private String empathy;
+
+	public String getEmpathy() {
+		return empathy;
+	}
+
+	public void setEmpathy(String empathy) {
+		this.empathy = empathy;
+	}
+
+}
---
+++ SpringPRJ - 복사본/src/poly/persistance/mongo/IEmpathyMapper.java
@@ -0,0 +1,21 @@
+package poly.persistance.mongo;
+
+import poly.dto.EmpathyDTO;
+
+public interface IEmpathyMapper {
+
+	/*
+	 * MongoDB 컬렉션 생성
+	 * 
+	 * @param colNm 생성하는 컬렉션 이름
+	 */
+	
+	public boolean createCollection(String colNm) throws Exception;
+	
+	/*
+	 * MongoDB 데이터 저장하기
+	 * 
+	 * @param pDTO 저장될 정보
+	 */
+	public int insertEmpathy(EmpathyDTO pDTO, String colNm) throws Exception;
+}
